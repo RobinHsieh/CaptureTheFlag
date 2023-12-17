f40ab0a6ab08fce2fc5af115ad8bc9e307dec732
RobinHsieh <robin0623.hsieh@gmail.com>
2023-12-17 09:55:43 +0000

    update try.py

--- 06_shellcode2/instructionSetTry/try.py
+++ 06_shellcode2/instructionSetTry/try.py
@@ -1,7 +1,8 @@
 from pwn import *
 context.update(arch='amd64', os='linux')
 print(disasm(b"\x0c\x87\x63"))
+print()
 for i in range(0x100):
-    # print(disasm(b"\x0c\x87\x63" + bytes([i])))
+    print(disasm(b"\x0c\x87\x63" + bytes([i])))
     print()
 
